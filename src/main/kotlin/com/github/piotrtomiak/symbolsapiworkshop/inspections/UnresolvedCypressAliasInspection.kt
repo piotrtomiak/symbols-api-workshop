02f25db55706eb92aba3fe3e4bc46c971f77eeb4
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-03-26 13:04:23 +0100

    Implement unresolved reference inspection

--- src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/inspections/UnresolvedCypressAliasInspection.kt
+++ src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/inspections/UnresolvedCypressAliasInspection.kt
@@ -1,5 +1,8 @@
+@file:Suppress("UnstableApiUsage")
+
 package com.github.piotrtomiak.symbolsapiworkshop.inspections
 
+import com.github.piotrtomiak.symbolsapiworkshop.CypressAliasSymbolReference
 import com.github.piotrtomiak.symbolsapiworkshop.MyBundle
 import com.github.piotrtomiak.symbolsapiworkshop.cypressAliasName
 import com.intellij.codeHighlighting.HighlightDisplayLevel
@@ -8,6 +11,8 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.lang.javascript.psi.JSElementVisitor
 import com.intellij.lang.javascript.psi.JSLiteralExpression
+import com.intellij.model.psi.PsiSymbolReferenceService
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElementVisitor
 
 class UnresolvedCypressAliasInspection : LocalInspectionTool() {
@@ -18,7 +23,10 @@ class UnresolvedCypressAliasInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
         object : JSElementVisitor() {
             override fun visitJSLiteralExpression(node: JSLiteralExpression) {
-                // TODO
+                PsiSymbolReferenceService.getService()
+                    .getReferences(node, CypressAliasSymbolReference::class.java)
+                    .filter { it.resolveReference().isEmpty() }
+                    .forEach { registerUnresolvedReferenceProblem(holder, node) }
             }
 
         }
@@ -27,7 +35,8 @@ class UnresolvedCypressAliasInspection : LocalInspectionTool() {
         holder.registerProblem(
             node,
             MyBundle.message("cy.codeInsight.inspection.unresolvedAlias.message", node.cypressAliasName!!),
-            ProblemHighlightType.GENERIC_ERROR
+            ProblemHighlightType.GENERIC_ERROR,
+            TextRange(2, node.text.lastIndex),
         )
     }
 
