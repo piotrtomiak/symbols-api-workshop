6f70c91d78d1c6fcb454573ef4e20671da88f645
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-04-09 14:17:19 +0200

    Add support for code completion

--- src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/CypressAliasSymbolReference.kt
+++ src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/CypressAliasSymbolReference.kt
@@ -2,6 +2,12 @@
 
 package com.github.piotrtomiak.symbolsapiworkshop
 
+import com.intellij.codeInsight.completion.PrioritizedLookupElement
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.icons.AllIcons
+import com.intellij.lang.javascript.completion.JSLookupPriority
+import com.intellij.model.psi.PsiCompletableReference
 import com.intellij.model.psi.PsiSymbolReference
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
@@ -9,7 +15,7 @@ import com.intellij.psi.PsiElement
 class CypressAliasSymbolReference(
     private val element: PsiElement,
     private val textRangeInElement: TextRange
-) : PsiSymbolReference {
+) : PsiSymbolReference, PsiCompletableReference {
 
     override fun getElement(): PsiElement = element
 
@@ -22,5 +28,21 @@ class CypressAliasSymbolReference(
             .filter { it.cypressAliasName == referencedName }
             .map { CypressAliasSymbol(referencedName, it) }
     }
+
+    override fun getCompletionVariants(): Collection<LookupElement> =
+        getCypressAliasesAvailiableForContext(element).mapNotNull { declaration ->
+            val lookupElement =
+                LookupElementBuilder.create(
+                    declaration.cypressAliasName?.takeIf { it.isNotBlank() }
+                        ?: return@mapNotNull null
+                )
+                    .withIcon(AllIcons.Nodes.Variable)
+                    .withTailText(" cypress alias", true)
+
+            PrioritizedLookupElement.withPriority(
+                lookupElement,
+                JSLookupPriority.SMART_PRIORITY.priorityValue.toDouble()
+            )
+        }
 
 }
