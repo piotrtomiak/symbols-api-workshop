99db440b6edf324dbf3d0db4f4223a226babbf36
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-04-16 14:11:08 +0200

    Add naive support for `this.<aliasName>` syntax

--- src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/CypressAliasSymbolReferenceProvider.kt
+++ src/main/kotlin/com/github/piotrtomiak/symbolsapiworkshop/CypressAliasSymbolReferenceProvider.kt
@@ -3,6 +3,8 @@
 package com.github.piotrtomiak.symbolsapiworkshop
 
 import com.intellij.lang.javascript.psi.JSLiteralExpression
+import com.intellij.lang.javascript.psi.JSReferenceExpression
+import com.intellij.lang.javascript.psi.JSThisExpression
 import com.intellij.model.Symbol
 import com.intellij.model.psi.PsiExternalReferenceHost
 import com.intellij.model.psi.PsiSymbolReference
@@ -12,16 +14,24 @@ import com.intellij.model.search.SearchRequest
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 
-class CypressAliasSymbolReferenceProvider: PsiSymbolReferenceProvider {
+class CypressAliasSymbolReferenceProvider : PsiSymbolReferenceProvider {
 
     override fun getReferences(
         element: PsiExternalReferenceHost,
         hints: PsiSymbolReferenceHints,
     ): Collection<PsiSymbolReference> =
-        if (element is JSLiteralExpression && element.isCypressGetOrWaitAliasUsage)
-           listOf(CypressAliasSymbolReference(element, TextRange(2, element.text.lastIndex)))
-        else
-            emptyList()
+        when {
+            element is JSLiteralExpression && element.isCypressGetOrWaitAliasUsage ->
+                listOf(CypressAliasSymbolReference(element, TextRange(2, element.text.lastIndex)))
+
+            element is JSReferenceExpression && element.qualifier is JSThisExpression ->
+                element.referenceNameElement
+                    ?.textRangeInParent
+                    ?.let { listOf(CypressAliasSymbolReference(element, it)) }
+                    ?: emptyList()
+
+            else -> emptyList()
+        }
 
     override fun getSearchRequests(
         project: Project,
